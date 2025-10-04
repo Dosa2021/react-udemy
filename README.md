28cfac7807f39a79c87693b6f0fcdfc915562d22
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-10-04 15:04:41 +0900

    コンポーネントの再作成（App.jsx）

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 npm start
+
+・JSX
+・コンポーネント
+　・一般的？にはファイル名は〜.jsx
+・Reactでのイベント・スタイル
+
+【参考】
+・reactバージョン変更
+https://qiita.com/tsubasa_k0814/items/d90254c807c7280c2db7
---
+++ react-app/src/App.jsx
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>hoge/</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export const App = () => {
+  return (
+    // React.fragment or 空タグ
+    <>
+      <h1>hh</h1>
+      <p>hogehogefuga</p>
+    </>
+  );
+};
